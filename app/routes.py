0bc0200e031ede241d6b8703a0e3dbbeb1bfce7c
Kimberly Lantigua <klantigua11@gmail.com>
2023-04-20 11:47:05 -0700

    Created the Planet class and a list of planet objects

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint
 
+class Planet: 
+
+    def __init__(self, id, name, description, position): 
+        self.id = id
+        self.name = name
+        self.descrption = description 
+        self.position = position 
+
+earth = Planet(6, "earth", "only planet known with liquid water", 3)
+mars = Planet(67, "mars", "dusty cold desert world", 4)
+venus = Planet(89, "venus", "hot girl planet", 2)
+
+planet_list = [earth, mars, venus]
